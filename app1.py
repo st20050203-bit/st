cf520f79ddec34796e73206d59bede6865ca766d
st20050203-bit <st.20050203@gmail.com>
2025-11-20 16:20:08 +0900

    Update app1.py

--- app1.py
+++ app1.py
@@ -2,4 +2,10 @@ import streamlit as st
 
 st.title("こんにちは、吉村ゼミ")
 
-st.text_input("名前を入力してください")
+name = st.text_input("好きな言葉を入力してください")
+
+st.write(name)
+
+camera = st.camera_input("写真を撮影します!")
+if camera:
+  st.image(camera,caption = "写真", use_column_width=True)
