4c21a0d5d7989f3f2c55a99f1d94171ea963d92f
st20050203-bit <st.20050203@gmail.com>
2025-11-27 15:35:01 +0900

    Update app1.py

--- app1.py
+++ app1.py
@@ -11,6 +11,10 @@ st.write(address)
 
 hobby = st.multiselect("次から複数選択してください",["映画","音楽","散歩"])
 st.write(hobby)
+
+st.slider("この映画を10点満点で評価してください",0,10,0)
+
+
 camera = st.camera_input("写真を撮影します!")
 if camera:
   st.image(camera,caption = "写真", use_column_width=True)
