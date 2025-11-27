7acf9b61718a86501d3c7d3448af67b55bc3a9ab
st20050203-bit <st.20050203@gmail.com>
2025-11-27 15:40:44 +0900

    Update app1.py

--- app1.py
+++ app1.py
@@ -16,6 +16,8 @@ score = st.slider("この映画を10点満点で評価してください",0,10,5
 st.write(score)
 
 st.radio("性別を選択してください",["男性","女性"])
+
+
 camera = st.camera_input("写真を撮影します!")
 if camera:
   st.image(camera,caption = "写真", use_column_width=True)
