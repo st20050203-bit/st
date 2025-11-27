8992c2667c3c4c7eb9f59d009a2bb136724f47ff
st20050203-bit <st.20050203@gmail.com>
2025-11-27 15:27:38 +0900

    Update app1.py

--- app1.py
+++ app1.py
@@ -6,7 +6,8 @@ name = st.text_input("好きな言葉を入力してください")
 
 st.write(name)
 st.checkbox("同意します")
-st.selectbox("次の中から現住所を教えてください",["京都府","大阪府"])
+address=st.selectbox("次の中から現住所を教えてください",["京都府","大阪府"])
+st.write(address)
 camera = st.camera_input("写真を撮影します!")
 if camera:
   st.image(camera,caption = "写真", use_column_width=True)
