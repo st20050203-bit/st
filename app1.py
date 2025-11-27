d997c950c595c9e42ad4352148dde8406db580a9
st20050203-bit <st.20050203@gmail.com>
2025-11-27 15:31:36 +0900

    Update app1.py

--- app1.py
+++ app1.py
@@ -9,7 +9,8 @@ st.checkbox("同意します")
 address=st.selectbox("次の中から現住所を教えてください",["京都府","大阪府"])
 st.write(address)
 
-st.multiselect("次から複数選択してください",["映画","音楽","散歩"])
+hobby = st.multiselect("次から複数選択してください",["映画","音楽","散歩"])
+st.writw(hobby)
 camera = st.camera_input("写真を撮影します!")
 if camera:
   st.image(camera,caption = "写真", use_column_width=True)
