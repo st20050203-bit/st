8fe05ac54b93294a64157e7045431c302a478532
st20050203-bit <st.20050203@gmail.com>
2025-11-27 15:20:39 +0900

    Update app1.py

--- app1.py
+++ app1.py
@@ -5,7 +5,7 @@ st.title("こんにちは、吉村ゼミ")
 name = st.text_input("好きな言葉を入力してください")
 
 st.write(name)
-
+st.checkbox("同意します")
 camera = st.camera_input("写真を撮影します!")
 if camera:
   st.image(camera,caption = "写真", use_column_width=True)
